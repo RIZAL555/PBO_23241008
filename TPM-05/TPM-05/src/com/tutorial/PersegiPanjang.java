bd0bba4217021ff226602b4b1de10bd56910226f
RIZAL555 <147357342+RIZAL555@users.noreply.github.com>
2024-12-21 13:23:29 +0700

    Add files via upload

---
+++ TPM-05/TPM-05/src/com/tutorial/PersegiPanjang.java
@@ -0,0 +1,19 @@
+package com.tutorial;
+
+public class PersegiPanjang extends BangunDatar {
+    float panjang;
+    float lebar;
+
+    public PersegiPanjang(float panjang, float lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+
+    public float luas() {
+        return panjang * lebar;
+    }
+
+    public float keliling() {
+        return 2 * (panjang + lebar);
+    }
+}
